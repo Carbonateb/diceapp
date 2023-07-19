80a7d57158c12fed40a1a7784e9f3f2dc820066c
Lucas Champagne <31268463+Carbonateb@users.noreply.github.com>
2023-07-19 12:22:19 +1000

    Added fakeouts

--- src/routes/Dice.svelte
+++ src/routes/Dice.svelte
@@ -8,12 +8,70 @@
 	const animationDelayMs = 120;
 
 	let rootDiv: HTMLDivElement;
+
+	const isMaxed = value == maxValue;
+	const isOne = value == 1;
+
+	let suspense = false;
+	let fakeOne = false;
+	let fakeMaxed = false;
+
+	/** A number between 0 and 1 */
+	let rng = Math.random();
+
+	if (isMaxed) {
+		// To max fakeouts (exciting)
+		// 10% chance of faking a One
+		// 20% chance of faking a normal
+		// 10% chance of faking a fakeout
+		if (rng < 0.1) {
+			suspense = true;
+			fakeOne = true;
+		} else if (rng < 0.3) {
+			suspense = true;
+		} else if (rng < 0.4) {
+			suspense = true;
+			fakeMaxed = true;
+		}
+	} else if (isOne) {
+		// To one fakeouts (frustrating)
+		// 10% chance of faking a Max
+		// 20% chance of faking a normal
+		// 10% chance of faking a fakeout
+		if (rng < 0.1) {
+			suspense = true;
+			fakeMaxed = true;
+		} else if (rng < 0.3) {
+			suspense = true;
+		} else if (rng < 0.4) {
+			suspense = true;
+			fakeOne = true;
+		}
+	} else {
+		// Normal fake outs
+		// 5% chance of faking a Max
+		// 10% chance of faking a one
+		// 10% chance of faking a fakeout
+		if (rng < 0.05) {
+			suspense = true;
+			fakeMaxed = true;
+		} else if (rng < 0.15) {
+			suspense = true;
+			fakeOne = true;
+		} else if (rng < 0.25) {
+			suspense = true;
+		}
+	}
 </script>
 
 <div
 	bind:this={rootDiv}
 	class="flex h-28 w-28 items-center justify-center rounded-full bg-orange-950/0 text-8xl font-medium"
-	class:maxed={value == maxValue}
+	class:maxed={isMaxed}
+	class:one={isOne}
+	class:suspense
+	class:fake-maxed={fakeMaxed}
+	class:fake-one={fakeOne}
 	style="animation-delay: {(2 + index) * animationDelayMs}ms;"
 >
 	{value}
@@ -24,12 +82,28 @@
 		animation: appear 1200ms both cubic-bezier(0, -0, 0, 1);
 		font-family: "Expletus Sans";
 		--colour: theme(colors.slate.50);
-		--glow-colour: transparent;
 	}
 
-	div.maxed {
+	.suspense {
+		animation-name: appear-suspense;
+		animation-duration: 2400ms; /* Double normal */
+		--fakeout-color: theme(colors.slate.50);
+	}
+
+	.maxed {
 		--colour: theme(colors.orange.500);
-		--glow-colour: theme(colors.orange.600);
+	}
+
+	.one {
+		--colour: theme(colors.slate.400);
+	}
+
+	.fake-maxed {
+		--fakeout-color: theme(colors.orange.500);
+	}
+
+	.fake-one {
+		--fakeout-color: theme(colors.slate.400);
 	}
 
 	@keyframes appear {
@@ -38,7 +112,6 @@
 			opacity: 0;
 			background-color: var(--colour);
 			color: transparent;
-			box-shadow: 0 0 0px var(--glow-colour);
 		}
 		25% {
 			opacity: 1;
@@ -48,7 +121,6 @@
 			transform: translateY(0) scale(0.5);
 			background-color: var(--colour);
 			color: transparent;
-			box-shadow: 0 0 25px var(--glow-colour);
 		}
 		71% {
 			color: var(--colour);
@@ -56,8 +128,38 @@
 		100% {
 			transform: translateY(0) scale(1);
 			background-color: transparent;
+		}
+	}
 
-			box-shadow: 0 0 0px var(--glow-colour);
+	@keyframes appear-suspense {
+		0% {
+			transform: translateY(50px) scale(0.5);
+			opacity: 0;
+			background-color: var(--fakeout-color);
+			color: transparent;
+		}
+		12.5% {
+			opacity: 1;
+		}
+		20%,
+		35% {
+			transform: translateY(0) scale(0.5);
+			background-color: var(--fakeout-color);
+		}
+		65% {
+			background-color: var(--fakeout-color);
+		}
+		85% {
+			transform: translateY(0) scale(0.7);
+			background-color: var(--colour);
+			color: transparent;
+		}
+		86% {
+			color: var(--colour);
+		}
+		100% {
+			transform: translateY(0) scale(1);
+			background-color: transparent;
 		}
 	}
 </style>
